811f921fdd3da388795883188090dbf7e335b22f
mimr509 <mimr509@gmail.com>
2020-07-17 03:36:06 +0600

    feetToMile done

---
+++ assigment.js
@@ -0,0 +1,10 @@
+// Start feetToMile function work
+      var feet = 20000;
+      function feetToMile(feet){
+      var mile = feet  / 5280;
+        return mile ;
+      }
+      var result = feetToMile(20000);
+      console.log(result.toFixed(2));
+ 
+// End first task
